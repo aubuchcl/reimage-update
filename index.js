a750b214f28fa8b1c9510d16daee611b75bd117a
Chris Aubuchon <aubuchcl@gmail.com>
2024-07-09 16:42:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Version 15");
+  response.end("Version 16");
   console.log("Got a connection");
 });
 server.listen(80, "::0");
